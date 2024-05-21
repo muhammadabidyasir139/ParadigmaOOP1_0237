514ac83ca397bc97a79133331940d6d13f57d43f
wesclicdev <wesclicdev@gmail.com>
2024-05-21 15:45:18 +0700

    membuat inisialisasi pada folder agreagsi

---
+++ agregasi/anak.h
@@ -0,0 +1,10 @@
+#ifndef ANAK_H
+#define ANAK_H
+
+class anak {
+    public:
+        string nama;
+        anak(string pnama) :nama(pNama) {
+            cout << "Anak \" "<< nama <<"\"ada\n";
+        }
+}
